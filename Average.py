65c847d08ba3f1cf43baa291d7e2830a316264b4
JASPER SHARMA <96339698+Pettyman123@users.noreply.github.com>
2023-11-22 07:24:54 +0530

    Average.py
    with comments

--- Average.py
+++ Average.py
@@ -1,11 +1,11 @@
 a = int(input("Enter the number of values:"))
 
-b=[]
+b=[] #empty list
 for i in range (0,a):
     c= int(input("enter the number"))
     b.append(c)
     
-print(b)
-sum = sum(b)
+print(b)#The list of all input values
+sum = sum(b) #sum function adds all the entites of the list
 
 print("The average is",sum/a)
