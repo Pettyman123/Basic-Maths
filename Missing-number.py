39ea6ea5b83637615ef140d887dcec36ddd66dbd
JASPER SHARMA <96339698+Pettyman123@users.noreply.github.com>
2023-11-23 17:35:07 +0530

    Add files via upload

---
+++ Missing-number.py
@@ -0,0 +1,8 @@
+a = float(input("Enter the numbers LHS:"))
+b = float(input("Enter the numbers RHS:"))
+x = int(input("The number between LHS and RHS:"))
+
+if x> a and x<b:
+    print("Correct")
+else:
+    print("Incorrect")
