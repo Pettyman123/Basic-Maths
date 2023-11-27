6ef1bc1f52989788b5a19f5d81e4d3d1892fdee2
JASPER SHARMA <96339698+Pettyman123@users.noreply.github.com>
2023-11-27 11:00:49 +0530

    Update Compound-interest.py
    minute change

--- Compound-interest.py
+++ Compound-interest.py
@@ -1,8 +1,9 @@
 import math
-P	=	float(input("Initial principal balance:"))
-r	=	float(input("Interest rate:"))
-n	=	float(input("Time periods:"))
-
+def info():
+    P	=	float(input("Initial principal balance:"))
+    r	=	float(input("Interest rate:"))
+    n	=	float(input("Time periods:"))
+info()
 rate = (1+ r/100)**(n)
 ci = P* rate - P
 print("The compound interest is", ci)
